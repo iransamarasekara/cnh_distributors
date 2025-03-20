3cdb176cef63e8e7294519f29d7c994e1aaf863e
Chameera KHD <dinith.22@cse.mrt.ac.lk>
2025-03-20 13:43:50 +0530

    Logo Add

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import LoginForm from "../components/LoginForm";
 import { login_background } from "../assets";
+import logo from "../assets/logo_icon.png";
 
 const LoginPage = () => {
   return (
     <div className="flex h-screen bg-gray-50">
       <div className="m-auto w-full max-w-5xl flex overflow-hidden gap-6">
         <div className="w-full md:w-2/5 p-8 flex flex-col justify-center border-3 border-gray-200 rounded-3xl text-center">
-          <h1 className="text-3xl font-semibold text-blue-400 mb-1">
-            C&H DISTRIBUTORS
-          </h1>
-          <p className="text-gray-600 mb-20">Inventory Management System</p>
+          <div className="flex items-center justify-center gap-4 mb-2">
+            <img src={logo} alt="Zenden Logo" className="w-20 h-20" />
+            <div className="text-left">
+              <h1 className="text-4xl font-bold text-blue-400">ZENDEN</h1>
+              <p className="text-blue-400 font-medium text-xs tracking-widest">DIGITAL SOLUTIONS</p>
+            </div>
+          </div>
+          <p className="text-gray-600 mb-24">Inventory Management System</p>
           <LoginForm />
         </div>
         <div className="hidden md:block md:w-3/5">
